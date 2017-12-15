7572c24448d245b2d035d4a54c9067d48593d601
Nicolas Clavaud <nclavaud@gmail.com>
2017-12-15 10:51:47 +0100

    Use react

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-document.write('Hello!');
+import React from 'react'
+import { render } from 'react-dom'
+
+render(
+    <div>
+        <h1>Mes catalogues</h1>
+    </div>,
+    document.getElementById('root')
+);
